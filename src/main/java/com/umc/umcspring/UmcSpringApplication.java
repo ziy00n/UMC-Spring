ea77f862b0c9bd281cdaaee1fb8660fd4f1abcd3
Jiyun Kim <jiyun4188@gmail.com>
2023-05-15 19:39:04 +0900

    initial commit

---
+++ src/main/java/com/umc/umcspring/UmcSpringApplication.java
@@ -0,0 +1,13 @@
+package com.umc.umcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcSpringApplication.class, args);
+	}
+
+}
